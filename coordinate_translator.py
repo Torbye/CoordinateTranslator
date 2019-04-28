9da5d6bbcdf90df30da9a687f9c5de1d20270fdd
Torjus Bye <38226718+Torbye@users.noreply.github.com>
2019-04-28 19:46:09 +0200

    Update coordinate_translator.py

--- coordinate_translator.py
+++ coordinate_translator.py
@@ -2,23 +2,23 @@
 
 import math
 
-def car2pol(coordinate_list): #Converts cartesian coordinate_listdinates to polar [X, Y] -> [Radius, Angle].
+def car2pol(coordinate_list): #Converts cartesian coordinates to polar [X, Y] -> [Radius, Angle].
     x=coordinate_list[0]
     y=coordinate_list[1]
     return [math.sqrt(math.pow(x,2)+math.pow(y,2)), math.degrees(math.atan2(y,x))]
 
-def pol2car(coordinate_list): #Converts polar coordinate_listdinates to cartesian [Radius, Angle] -> [X, Y].
+def pol2car(coordinate_list): #Converts polar coordinates to cartesian [Radius, Angle] -> [X, Y].
     r=coordinate_list[0]
     a=math.radians(coordinate_list[1])
     return [r*math.cos(a), r*math.sin(a)]
 
-def sph2car(coordinate_list):  #Converts spherical coordinate_listdinates to cartesian [Radius, Inclination, Azimuth] -> [X, Y, Z].
+def sph2car(coordinate_list):  #Converts spherical coordiates to cartesian [Radius, Inclination, Azimuth] -> [X, Y, Z].
     i = pol2car([coordinate_list[0], coordinate_list[1]])
     z = i[1]
     i = pol2car([i[0], coordinate_list[2]])
     return [i[0], i[1], z]
 
-def car2sph(coordinate_list):  #Converts cartesian to spherical coordinate_listdinates [X, Y, Z] -> [Radius, Inclination, Azimuth].
+def car2sph(coordinate_list):  #Converts cartesian to spherical coordinates [X, Y, Z] -> [Radius, Inclination, Azimuth].
     x = coordinate_list[0]
     y = coordinate_list[1]
     z = coordinate_list[2]
